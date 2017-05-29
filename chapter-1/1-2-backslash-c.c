a9865d3e8694bfe5f94e69fd87d6d90798578936
Li Zhineng <lizhineng@gmail.com>
2017-05-29 08:28:34 +0800

    Fix misspelling in execrise 2 in chapter 1

--- chapter-1/1-2-backslash-c.c
+++ chapter-1/1-2-backslash-c.c
@@ -1,7 +1,7 @@
 /**
  * Exercise 1-2
  *
- * Experiment to find out what happens when prints's argument string contains \c,
+ * Experiment to find out what happens when printfs's argument string contains \c,
  * where c is some character not listed above.
  *
  * The C Programming Language - the second edition
