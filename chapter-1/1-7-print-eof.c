703291a76776623a34088131d3dc4bca397c6c8b
Li Zhineng <lizhineng@gmail.com>
2017-05-31 18:01:17 +0800

    Add exercise 1-7

---
+++ chapter-1/1-7-print-eof.c
@@ -0,0 +1,20 @@
+/**
+ * Exercise 1-7, page 17
+ *
+ * Write a program to print the value of EOF.
+ *
+ * The C Programming Language - the second edition
+ * by Brian Kernighan and Dennis Ritchie
+ *
+ * Author: Li Zhineng <lizhineng@gmail.com>
+ * URL:    https://zhineng.li
+ * File:   1-7-verify-eof.c
+ * Date:   2017-05-31
+ */
+
+#include <stdio.h>
+
+main()
+{
+    printf("%d\n", EOF);
+}
