984694a86282e6cda2543b59f4c5920f431fb114
Li Zhineng <im@zhineng.li>
2025-06-30 21:55:41 +0800

    add question

--- chapter-1/1-19/reverse.c
+++ chapter-1/1-19/reverse.c
@@ -1,3 +1,10 @@
+/**
+ * Exercise 1-19, page 31
+ *
+ * Write a function reverse(s) that reverses the character string s. Use it
+ * to write a program that reverses its input a line at a time.
+ */
+
 #include <stdio.h>
 #define MAXLINE 1000
 
